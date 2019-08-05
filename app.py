ee300aec6d262dd2edee9ca818cc8a611c423f71
Terry Drymonacos <terrdry@gmail.com>
2019-08-04 22:08:44 -0700

    CRUD support
    Signed-off-by: Terry Drymonacos <terrdry@gmail.com>

--- app.py
+++ app.py
@@ -1,25 +1,34 @@
-from flask import Flask,  jsonify
+from flask import Flask,  jsonify, request
 from flask_cors import CORS
+import uuid
 
 BOOKS = [
     {
+        'id': uuid.uuid4().hex,
         'title': 'The Fifth Season',
         'author': 'N.K. Jemesin',
         'reae': True
     },
     {
+        'id': uuid.uuid4().hex,
         'title': 'The Warrior who carried Life',
         'author': 'Geoff Ryman',
         'reae': True
-    },    {
+    },
+    {
+        'id': uuid.uuid4().hex,
         'title': 'Words of Radiance',
         'author': 'Brandon Sanderson',
         'reae': True
-    },    {
+    },
+    {
+        'id': uuid.uuid4().hex,
         'title': 'Roadside Picnic',
         'author': 'Arkady Strugatsky',
         'reae': True
-    },    {
+    },
+    {
+        'id': uuid.uuid4().hex,
         'title': 'Perdido Street Station',
         'author': 'China Mieville',
         'reae': True
@@ -44,12 +53,47 @@ CORS(app, resources={r'/*': {'origins': '*'}})
 def ping_pong():
     return jsonify('poof!')
 
-@app.route('/books', methods=['GET'])
+@app.route('/books', methods=['GET', 'POST'])
 def all_books():
-    return jsonify({
-        'status': 'success',
-        'books': BOOKS
-    })
+    response_object = {'status': 'success'}
+    if request.method == 'POST':
+        post_data = request.get_json()
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book added!'
+    else:
+        response_object['books'] = BOOKS
+
+    return jsonify(response_object)
+
+@app.route('/books/<book_id>', methods=['PUT', 'DELETE'])
+def single_book(book_id):
+    response_object = {'status': 'success'}
+    if request.method == 'PUT':
+        post_data = request.get_json()
+        remove_book(book_id)
+        BOOKS.append({
+            'id': uuid.uuid4().hex,
+            'title': post_data.get('title'),
+            'author': post_data.get('author'),
+            'read': post_data.get('read')
+        })
+        response_object['message'] = 'Book Updated!'
+    if request.method == 'DELETE':
+        remove_book(book_id)
+        response_object['message'] = 'Book removed!'
+    return jsonify(response_object)
+
+def remove_book(book_id):
+    for book in BOOKS:
+        if book['id'] == book_id:
+            BOOKS.remove(book)
+            return True
+    return False
 
 
 
